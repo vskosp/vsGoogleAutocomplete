aecbb4af766f67737858ef0fd40e76b064633b08
pol_kos <pol_kos@ukr.net>
2015-05-23 20:03:26 +0300

    changed validator logic and directive name

--- src/validator/validators/vsGooglePlaceValidator.js
+++
@@ -1,9 +0,0 @@
-vsGoogleAutocomplete.factory('vsGooglePlace', ['vsGooglePlaceUtility', function(vsGooglePlaceUtility) {
-    function validator(place) {
-		return vsGooglePlaceUtility.isGooglePlace(place);
-	}
-	
-	return {
-	    validator: validator
-	};
-}]);
